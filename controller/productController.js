6087c640dbb2907c11113ab1f7b83bf24bf46d7c
6306rrmm <marohit789@gmail.com>
2025-08-03 15:29:16 +0530

    added

--- controller/productController.js
+++ controller/productController.js
@@ -1,4 +1,4 @@
-const {Product} = require('../models/Product');
+const Product = require('../models/product');
 const Category = require('../models/Category');
 
 exports.addProduct = async (req, res) => {
